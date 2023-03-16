2ed4b58e61a5934bd3570027bcb86c1a06dbe1ab
Hatice Yorukoglu <yorukogluhatice@gmail.com>
2023-03-16 14:19:47 -0400

    Started to build student details

--- client/src/pages/StudentDetails.vue
+++ client/src/pages/StudentDetails.vue
@@ -1,9 +1,24 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <section>
+      <div>
+        <h1>Student Details</h1>
+        <div v-for="detail in studentDetails" :key="detail.id"></div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-
+//import axios from 'axios'
+export default {
+  name: 'StudentDetails',
+  data: () => ({
+    studentDetails: null
+  }),
+  mounted() {
+    this.studentDetails()
+  },
+  methods: {}
+}
 </script>
