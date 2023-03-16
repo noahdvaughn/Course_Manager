30db05b2503da1231af661d4b1b9800ba9ba78b9
Noah Vaughn <noahdvaughn@gmail.com>
2023-03-16 12:20:36 -0400

    finished backend

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const AppRouter = require('./routes/AppRouter')
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/api', AppRouter)
+
+app.get('/', (req, res) => {
+  res.send('This is the base path!')
+})
+
+app.listen(PORT, () => console.log(`Server Running On Port ${PORT} . . . `))
--- routes/Student_CourseRouter.js
+++ routes/Student_CourseRouter.js
@@ -1,2 +1,16 @@
 const Router = require('express').Router()
 const controller = require('../controllers/Student_CourseController')
+
+Router.post('/create-student-course', controller.CreateStudent_Course)
+Router.get(
+  '/get-student-course-by-student/:student_id',
+  controller.GetStudent_CourseByStudentId
+)
+Router.get(
+  '/get-student-course-by-course/:course_id',
+  controller.GetStudent_CourseByCourseId
+)
+Router.delete(
+  '/delete-student-course/:studentcourse_id',
+  controller.DeleteStudent_Course
+)
