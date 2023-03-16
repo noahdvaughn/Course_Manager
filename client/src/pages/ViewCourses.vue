c788a38b11ac5be2720c22eba8e8a7a3973a04f8
Jakub <sorok028@umn.edu>
2023-03-16 15:47:42 -0500

    changed courserouter.js

--- client/src/pages/ViewCourses.vue
+++ client/src/pages/ViewCourses.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div v-if="courses">
         <h1>View Courses</h1>
         <div v-for="course in courses" :key="course.id">
             <CourseCard :name="course.name" />
         </div>
-        <CreateCourse/>
+        <CreateCourse />
     </div>
 </template>
   
@@ -29,7 +29,6 @@ export default {
         async getAllCourses() {
             const res = await axios.get('http://localhost:3001/api/course/get-all-courses')
             console.log(res.data);
-
             this.courses = res.data
         }
     }
