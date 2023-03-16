6837b63877f34c7bd2c3d911c78514f634e85707
Jakub <sorok028@umn.edu>
2023-03-16 10:06:19 -0500

    created vuecourses

---
+++ client/src/pages/ViewCourses.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h1>View Courses</h1>
+      <ul>
+        <li v-for="course in courses" :key="course.id">{{ course.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+    import Vue from 'vue';
+    import axios from 'axios';
+  
+    export default {
+      data() {
+        return {
+          courses: []
+        }
+      },
+      mounted() {
+        axios.get('/api/courses')
+          .then(response => {
+            this.courses = response.data;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    }
+  </script>
+  
