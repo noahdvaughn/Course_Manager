99703ecf80ddb6022cffef16a43149438cfcc9cd
Jakub <sorok028@umn.edu>
2023-03-16 11:10:17 -0500

    home page routes working

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-        <h1>Welcome to the Course Manager.</h1>
-        <h3>An app to keep track of your students and their grades.</h3>
-        <button @click="$router.push('/view-courses')">View Courses</button>
-        <button @click="$router.push('/view-students')">View Students</button>
-    </div>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Welcome to the Course Manager.</h1>
+    <h3>An app to keep track of your students and their grades.</h3>
+    <button @click="$router.push('/view-courses')">View Courses</button>
+    <button @click="$router.push('/view-students')">View Students</button>
+  </div>
 </template>
 
 <script>
 
-  export default {
-    name: 'HomePage'
-  }
+export default {
+  name: 'HomePage'
+}
 </script>
