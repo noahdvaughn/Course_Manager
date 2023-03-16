fdc5496f7b2ba3256e598c0d2e41661e242284d7
Hatice Yorukoglu <yorukogluhatice@gmail.com>
2023-03-16 12:17:50 -0400

    Student and course cards are ready.

---
+++ client/src/components/CourseCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <div class="info-wrapper flex-col">
+      <h3>{{ course.name }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CourseCard',
+  props: {
+    courses: {}
+  }
+}
+</script>
---
+++ client/src/components/StudentCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <div class="info-wrapper flex-col">
+      <h3>{{ student.name }}</h3>
+      <h3>{{ student.email }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StudentCard',
+  props: {
+    students: {}
+  }
+}
+</script>
