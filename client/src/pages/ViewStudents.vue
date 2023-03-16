b8adfbc097a73940a1def491e0915ff7b1633755
Jakub <sorok028@umn.edu>
2023-03-16 10:49:30 -0500

    created Home Page

---
+++ client/src/pages/ViewStudents.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>View Students</h1>
+      <ul>
+        <li v-for="student in students" :key="student.id">{{ student.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+    import axios from 'axios';
+  
+    export default {
+      data: () => ({
+          students: []
+        }),
+      mounted() {
+        this.getAllStudents()
+      },
+      methods: {
+        async getAllStudents() {
+            const res = await axios.get('/api/students')
+            this.students = res.data
+        }
+      }
+    }
+  </script>
+  
