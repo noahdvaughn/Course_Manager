c2843bed8ea5fbaaa1a0e44700413811cd66a3f7
Jakub <sorok028@umn.edu>
2023-03-17 08:07:51 -0500

    added bracket

--- client/src/components/CreateStudent_Course.vue
+++ client/src/components/CreateStudent_Course.vue
@@ -21,30 +21,23 @@ import axios from 'axios';
 // import CourseCard from './CourseCard.vue';
 export default {
 
-data: () => ({
-  courses: [],
-  courseSelected: null,
-  selectedId: 1,
-  grade: 'A',
-  mathNum: 0,
-  avgNum: 0,
-  student_name: ''
-}),
-props: {
-  student_id: Number,
-  my_student_name: String
-},
-mounted() {
-  this.student_name = this.my_student_name
-        console.log(this.my_student_name)
-        this.getCourses()
-        
-    },
-methods: {
-  async getCourses(){
-    const res = await axios.get('http://localhost:3001/api/course/get-all-courses')
-            this.courses = res.data
-            
+  data: () => ({
+    courses: [],
+    courseSelected: null,
+    selectedId: 1,
+    grade: 'A',
+    mathNum: 0,
+    avgNum: 0,
+    student_name: ''
+  }),
+  props: {
+    student_id: Number,
+    my_student_name: String
+  },
+  mounted() {
+    this.student_name = this.my_student_name
+    console.log(this.my_student_name)
+    this.getCourses()
 
   },
   methods: {
@@ -52,47 +45,55 @@ methods: {
       const res = await axios.get('http://localhost:3001/api/course/get-all-courses')
       this.courses = res.data
 
+
     },
-    async enrollInCourse() {
-      let score
-      switch (this.grade) {
-        case 'A':
-          score = 4;
-          break;
-        case 'B':
-          score = 3;
-          break;
-        case 'C':
-          score = 2;
-          break;
-        case 'D':
-          score = 1;
-          break;
-        case 'F':
-          score = 0;
-          break;
+    methods: {
+      async getCourses() {
+        const res = await axios.get('http://localhost:3001/api/course/get-all-courses')
+        this.courses = res.data
+
+      },
+      async enrollInCourse() {
+        let score
+        switch (this.grade) {
+          case 'A':
+            score = 4;
+            break;
+          case 'B':
+            score = 3;
+            break;
+          case 'C':
+            score = 2;
+            break;
+          case 'D':
+            score = 1;
+            break;
+          case 'F':
+            score = 0;
+            break;
+
+        }
+
+        const res = await axios.post('http://localhost:3001/api/student_course/create-student-course', {
+          score: score, letter: this.grade, student_name: this.student_name, course_name: this.courses[this.mathNum].name,
+          studentId: this.student_id, courseId: this.selectedId
+        })
+        console.log(res)
+      },
+      changeEnrolled(e) {
+
+        this.selectedId = parseInt(e.target.value)
+        this.mathNum = e.target.selectedIndex
+        console.log(this.grade)
+      },
+      async DeleteStudent() {
+        await axios.delete(`http://localhost:3001/api/student/delete-student/${this.student_id}`)
+        this.$router.push(`/students`)
 
       }
 
-      const res = await axios.post('http://localhost:3001/api/student_course/create-student-course', {
-        score: score, letter: this.grade, student_name: this.student_name, course_name: this.courses[this.mathNum].name,
-        studentId: this.student_id, courseId: this.selectedId
-      })
-      console.log(res)
     },
-    changeEnrolled(e) {
-
-      this.selectedId = parseInt(e.target.value)
-      this.mathNum = e.target.selectedIndex
-      console.log(this.grade)
-    },
-    async DeleteStudent() {
-      await axios.delete(`http://localhost:3001/api/student/delete-student/${this.student_id}`)
-      this.$router.push(`/students`)
-
-    }
-
-  },
-  components: {}
+    components: {}
+  }
 }
 </script>
