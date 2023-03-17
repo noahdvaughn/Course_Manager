f7a358e0b0f3309526dc0398bf5ebf4c179fd178
Noah Vaughn <noahdvaughn@gmail.com>
2023-03-16 20:06:34 -0400

    finished enroll function

--- client/src/components/CreateStudent_Course.vue
+++ client/src/components/CreateStudent_Course.vue
@@ -1,31 +1,83 @@
 <template>
   <div>
-      <h1>Enroll Student in Class below</h1>
-      <div  v-for="course in courses" :key="course.id">
-            <CourseCard :name="course.name" />
-        </div>
+      <p>Enroll Student in Class below:</p>
+      <select  @change="changeEnrolled">
+             <option v-for="course in courses" :value='course.id' :key="course.id">{{ course.name }}</option>
+        </select>
+        <select v-model="grade">
+          <option>A</option>
+          <option>B</option>
+          <option>C</option>
+          <option>D</option>
+          <option>F</option>
+        </select>
+        <button @click="enrollInCourse">Enroll</button>
   </div>
+  <button @click="DeleteStudent">Delete Student?</button>
 </template>
 
 <script>
 import axios from 'axios';
+// import CourseCard from './CourseCard.vue';
 export default {
 data: () => ({
-  courses: []
+  courses: [],
+  courseSelected: null,
+  selectedId: 1,
+  grade: 'A',
+  mathNum: 0,
+  avgNum: 0
 }),
+props: ['student_id', 'student_name'],
 mounted() {
-        this.getAllCourses()
+        this.getCourses()
+        
     },
 methods: {
   async getCourses(){
     const res = await axios.get('http://localhost:3001/api/course/get-all-courses')
-            console.log(res.data);
             this.courses = res.data
+            
   },
   async enrollInCourse() {
-    const res = await axios.get('http://localhost:3001/api')
+    let score 
+    switch (this.grade){
+      case 'A':
+        score = 4;
+        break;
+      case 'B':
+        score = 3;
+        break;
+      case 'C':
+        score = 2;
+        break;
+      case 'D':
+        score = 1;
+        break;
+      case 'F':
+        score = 0;
+        break;
+
+    } 
+
+    const res = await axios.post('http://localhost:3001/api/student_course/create-student-course', {
+      score: score, letter: this.grade, student_name: this.student_name, course_name: this.courses[this.mathNum].name,
+      studentId: this.student_id, courseId: this.selectedId
+    })
+    console.log(res)
+  },
+  changeEnrolled(e){
+    this.selectedId = parseInt(e.target.value) 
+    this.mathNum = this.selectedId - 1
+    console.log(this.grade)
+  },
+  async DeleteStudent(){
+    await axios.delete(`http://localhost:3001/api/student/delete-student/${this.student_id}`)
+    this.$router.push(`/students`)
+
   }
   
-}
+},
+components: {}
 }
 </script>
--- client/src/pages/StudentDetails.vue
+++ client/src/pages/StudentDetails.vue
@@ -4,47 +4,59 @@
       <div>
         <h1>Student Details</h1>
         <div v-for="detail in studentDetails" :key="detail.id">
-          <h1>{{ detail.name }}</h1>
-          <h2>{{ detail.email }}</h2>
+          <h3>{{ detail.name }}-&nbsp;{{ detail.email }}</h3>
         </div>
         <div v-for="course in courses" :key="course.id">
-          <h2>{{ course.course_name }}</h2>
-          <h2>{{ course.letter }}</h2>
+          <h2>{{ course.course_name }}:&nbsp;{{ course.letter }}</h2>
         </div>
       </div>
     </section>
+    <div v-if="student_id">
+      <CreateStudent_Course  :student_id="student_id" :student_name="student_name"/>
+    </div>
   </div>
 </template>
 
 <script>
 //import { assertExpressionStatement } from '@babel/types'
 
+import CreateStudent_Course from '@/components/CreateStudent_Course.vue'
 import axios from 'axios'
 export default {
-  name: 'StudentDetails',
-  data: () => ({
-    studentDetails: [],
-    courses: []
-  }),
-  mounted() {
-    let studentId = parseInt(this.$route.params.student_id)
-    this.getStudentDetails(studentId)
-    this.getCourses(studentId)
-  },
-  methods: {
-    async getStudentDetails(studentId) {
-      const response = await axios.get(
-        `http://localhost:3001/api/student/get-student-by-id/${studentId}`
-      )
-      this.studentDetails = response.data
-      console.log(response.data)
+    name: "StudentDetails",
+    data: () => ({
+        studentDetails: [],
+        courses: [],
+        student_id: null,
+        gpaNum: 0
+    }),
+    mounted() {
+        let studentId = parseInt(this.$route.params.student_id);
+        this.getStudentDetails(studentId);
+        this.getCourses(studentId)
+        this.student_id = studentId;
+        this.doMath();
     },
-    async getCourses(studentId) {
-      const response = await axios.get(
-        `http://localhost:3001/api/student_course/get-student-course-by-student/${studentId}`
-      )
-      this.courses = response.data
-    }
-  }
+    methods: {
+        async getStudentDetails(studentId) {
+            const response = await axios.get(`http://localhost:3001/api/student/get-student-by-id/${studentId}`);
+            this.studentDetails = response.data;
+        },
+        async getCourses(studentId) {
+            const response = await axios.get(`http://localhost:3001/api/student_course/get-student-course-by-student/${studentId}`);
+            this.courses = response.data;
+            
+        },
+        doMath(){
+          let courseArray = this.courses
+          console.log(courseArray)
+          courseArray.forEach((e)=>{
+            this.gpaNum += e.score
+          })
+          this.gpaNum = this.gpaNum / courseArray.length
+          console.log(this.gpaNum)
+        }
+    },
+    components: { CreateStudent_Course }
 }
 </script>
