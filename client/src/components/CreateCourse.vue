d8ebc53e8a4616e1b9aa16f54aebe985914285fe
Hatice Yorukoglu <yorukogluhatice@gmail.com>
2023-03-17 09:22:24 -0400

    nothing

--- client/src/components/CreateCourse.vue
+++ client/src/components/CreateCourse.vue
@@ -1,30 +1,47 @@
 <template>
   <div>
     <form @submit="CreateCourse">
-      <h4>Add Course below</h4>
-      <input v-model="course_name">
-      <button>Add Course</button>
+      <h1 class="enter">Add Course below</h1>
+      <input placeholder="Course Name" v-model="course_name" />
+      <button class="addcourse">Add Course</button>
     </form>
-
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
   name: 'CreateCourse',
   data: () => ({
-      course_name: ''
-    }),
-  props: {
-    
-  },
+    course_name: ''
+  }),
+  props: {},
   methods: {
-    async CreateCourse(){
-      let response = await axios.post(`http://localhost:3001/api/course/create-course`, {name: this.course_name} )
+    async CreateCourse() {
+      let response = await axios.post(
+        `http://localhost:3001/api/course/create-course`,
+        { name: this.course_name }
+      )
       console.log(response)
     }
   }
 }
 </script>
+
+<style>
+.addcourse {
+  background-color: rgba(194, 235, 82, 0.372);
+  color: #5d45f6;
+  font-weight: bold;
+  padding: 0.2em;
+  width: 150px;
+  /* height: 20px; */
+  font-size: 20px;
+  border: 4px solid #5d45f6;
+  border-radius: 10px;
+  cursor: pointer;
+  margin-top: 20px;
+  margin-left: 10px;
+}
+</style>
--- client/src/pages/CourseDetails.vue
+++ client/src/pages/CourseDetails.vue
@@ -1,49 +1,68 @@
 <template>
-    <div>
-        <h1 v-if="course[0]"> {{ course[0].name }}</h1>
-        <div v-for="student in students" :key="student.id">
-            {{ student.studentId }} - {{ student.letter }}
-        </div>
+  <div>
+    <h1 class="studenttitle">Course Details</h1>
+    <h3 class="who" v-if="course[0]">{{ course[0].name }}</h3>
+    <div v-for="student in students" :key="student.id">
+      <h3 class="letter">{{ student.studentId }} - {{ student.letter }}</h3>
     </div>
-    <div>
-        <button @click="DeleteCourse(this.course_id)">Delete Course</button>
-    </div>
+  </div>
+  <div>
+    <button class="delete" @click="DeleteCourse(this.course_id)">
+      Delete Course
+    </button>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
-    data: () => ({
-        course: {},
-        students: [],
-        course_id: 0
-    }),
-    mounted() {
-        let courseId = parseInt(this.$route.params.course_id)
-        console.log(courseId);
-        this.getCourseById(courseId)
-        this.course_id = courseId
-        this.getStudents(courseId)
+  data: () => ({
+    course: {},
+    students: [],
+    course_id: 0
+  }),
+  mounted() {
+    let courseId = parseInt(this.$route.params.course_id)
+    console.log(courseId)
+    this.getCourseById(courseId)
+    this.course_id = courseId
+    this.getStudents(courseId)
+  },
+  methods: {
+    async getCourseById(courseId) {
+      const res = await axios.get(
+        `http://localhost:3001/api/course/get-course-by-id/${courseId}`
+      )
+      this.course = res.data
     },
-    methods: {
-        async getCourseById(courseId) {
-            const res = await axios.get(`http://localhost:3001/api/course/get-course-by-id/${courseId}`)
-            this.course = res.data
-        },
-        async getStudents(courseId) {
-            const response = await axios.get(`http://localhost:3001/api/student_course/get-student-course-by-course/${courseId}`);
-            console.log(response);
-            this.students = response.data;
-
-        },
-        async DeleteCourse(courseId) {
-            await axios.delete(`http://localhost:3001/api/course/delete-course/${courseId}`)
-            this.$router.push('/courses')
-        }
-
-
-
+    async getStudents(courseId) {
+      const response = await axios.get(
+        `http://localhost:3001/api/student_course/get-student-course-by-course/${courseId}`
+      )
+      console.log(response)
+      this.students = response.data
+    },
+    async DeleteCourse(courseId) {
+      await axios.delete(
+        `http://localhost:3001/api/course/delete-course/${courseId}`
+      )
+      this.$router.push('/courses')
     }
+  }
 }
 </script>
+
+<style>
+.letter {
+  color: #5d45f6;
+  font-size: 20px;
+  padding: 1px;
+  border: 2px solid #5d45f6;
+  border-radius: 3px;
+  margin: 2px;
+  font-weight: bold;
+  width: 100px;
+  text-align: center;
+}
+</style>
